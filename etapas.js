0e6bf37555377994f2740ec0988af15bf19e6bdd
henriquevega <henriquevega.contato@gmail.com>
2021-06-03 17:44:41 -0300

    changes

---
+++ etapas.js
@@ -0,0 +1,49 @@
+let etapas = [
+{
+    titulos: `VEREADOR`,
+    numero: 5,
+    candidatos: [
+        {
+            numero: 38111,
+            nome: 'Fulano de Tal',
+            partido: 'ABC',
+            fotos: [
+                {url:vice-02.jpg, legenda: 'Vereador'},
+            ]
+        },
+        {
+            numero: 77222,
+            nome: 'Ciclano de Fulano',
+            partido: 'CBA',
+            fotos: [
+                {url:vice-01.jpg, legenda: 'Vereador'},
+            ]
+        },
+    ]
+},
+    {titulos: `Prefeito`,
+        numero: 2,
+        candidatos: [
+            {
+                numero: 38,
+                nome: 'Portuga',
+                partido: 'ABC',
+                fotos: [
+                    {url:prefeito-02.jpg, legenda: 'Prefeito'},
+                    {url:prefeito-03.jpg, legenda: 'Vice-Prefeito', small: true},
+            ]
+        },
+        {
+            numero: 17,
+            nome: 'Bolsonaro',
+            partido: 'PSL',
+            fotos: [
+                {url:prefeito-02.jpg, legenda: 'Prefeito'},
+                {url:prefeito-03.jpg, legenda: 'Vice-Prefeito', small: true},
+        ]
+        },
+        ]
+    }
+
+
+]
